eb6458a63fc08eff97ed69cfa2d9fb74c54993e3
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-02 12:30:37 +0300

    index.js: perform a GET

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 let x = document.createElement("pre")
 x.textContent = "Hello World"
+
+fetch("http://127.0.0.1/hello").then(function(response) {
+  return response.json();
+}).then(function(data) {
+  console.log(data);
+}).catch(function(err) {
+  console.log('Fetch Error :-S', err);
+});
+
+
 document.body.appendChild(x)
