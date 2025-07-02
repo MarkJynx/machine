6e9dd15b41e0fdaf5b1acb9e046aaa6bd75d1987
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-02 12:30:37 +0300

    index.js: perform a POST

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let x = document.createElement("pre")
 x.textContent = "Hello World"
 
-fetch("http://127.0.0.1/hello").then(function(response) {
-  return response.json();
+fetch("http://127.0.0.1/cgi-bin/hello", {method:"POST"}).then(function(response) {
+  return response.text();
 }).then(function(data) {
   console.log(data);
 }).catch(function(err) {
