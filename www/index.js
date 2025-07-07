adb5cede825d725c1cf60b00f8ca32eec94c4bb7
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-07 17:05:32 +0300

    hello.lua: rename

--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
 let x = document.createElement("pre")
 x.textContent = "Hello World"
 
-fetch("http://127.0.0.1/cgi-bin/hello", {method:"POST"}).then(function(response) {
+fetch("http://127.0.0.1/cgi-bin/hello.lua", {method:"POST"}).then(function(response) {
   return response.text();
 }).then(function(data) {
   console.log(data);
