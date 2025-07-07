232547f786cad974daed4e29ec62282def505b70
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-07 10:11:44 +0300

    init.sql: create task table

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS category (
 	name TEXT NOT NULL UNIQUE,
 	description TEXT NOT NULL UNIQUE,
 	motivation TEXT NOT NULL UNIQUE,
-	color INTEGER NOT NULL UNIQUE
+	color INTEGER NOT NULL UNIQUE CHECK(color >= 0 AND color <= 0xFFFFFF)
 );
 
 INSERT INTO category (name, description, motivation, color) VALUES (
@@ -22,4 +22,17 @@ INSERT INTO category (name, description, motivation, color) VALUES (
 	"Feeling well about one's surroundings."                      || char(10) ||
 	"Feeling well about how others perceive one's surroundings.",
 	0x000080
+);
+
+CREATE TABLE IF NOT EXISTS task (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL UNIQUE,
+	description TEXT NOT NULL UNIQUE,
+	motivation TEXT NOT NULL UNIQUE,
+	schedule_period INTEGER NOT NULL CHECK(schedule_period >= 1 AND schedule_period < 7), -- anything less frequent is not worthy to be a rule
+	schedule_weekdays INTEGER NOT NULL CHECK(schedule_weekdays >= 0 AND schedule_weekdays <= 127), -- 7-bit integer, LSB is Monday, MSB is Sunday
+	tier INTEGER NOT NULL CHECK(tier > 0),
+	order_priority INTEGER NOT NULL CHECK(order_priority > 0),
+	points INTEGER NOT NULL CHECK(points > 0),
+	color INTEGER NOT NULL CHECK(color >= 0 and color <= 0xFFFFFF)
 );
