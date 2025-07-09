f28853bfbfbf833c08981082c090ec7d680eb431
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-09 13:15:24 +0300

    struggle with opening a database

--- www/cgi-bin/read_day.lua
+++ www/cgi-bin/read_day.lua
@@ -1,6 +1,12 @@
 #!/usr/bin/env lua
 
+local DB_PATH = "my.db"
+
 local sqlite = require("luasql.sqlite3")
+local sqlite_env = sqlite.sqlite3() -- TODO: check for errors
+assert(sqlite_env)
+local sqlite_db = sqlite_env:connect(DB_PATH)
+assert(sqlite_db)
 
 local content_length = os.getenv("CONTENT_LENGTH")
 if content_length ~= nil then
@@ -15,14 +21,22 @@ if content_length > 0 then
 	payload = io.read(content_length)
 end
 
--- TODO: validate (match) payload
--- TODO: construct SQL query
--- TODO: execute SQL query
--- TODO: extract day
--- TODO: extract rule instaces
--- TODO: produce response
-
 local response = "null"
+
+os.execute("pwd 1>&2")
+if payload ~= nil and string.match(payload, "^%d%d%d%d-%d%d-%d%d$") then
+--	local query = "SELECT * FROM day WHERE id = '" .. payload .. "'"
+	local query = "SELECT id FROM day"
+	local result = sqlite_db:execute(query)
+	assert(result)
+	local row = result:fetch({}, "a")
+	if row then
+		response = "{}"
+		-- TODO: extract day
+		-- TODO: extract rule instaces
+		-- TODO: produce response
+	end
+end
 
 io.write("Status: 200 OK\r\n")
 io.write("Content-Type: application/json;charset=utf-8\r\n")
