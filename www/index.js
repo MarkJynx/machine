cfd3b8fb8acce92e52b3b92651f35c5628b84e1f
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-09 10:46:54 +0300

    index.js: cleanup front-end

--- www/index.js
+++ www/index.js
@@ -1,15 +1,3 @@
 let x = document.createElement("pre")
 x.textContent = "Hello World"
-
-//fetch("http://127.0.0.1/cgi-bin/hello.lua", {method:"POST", body: JSON.stringify({ username: "example" })}).then(function(response) {
-fetch("http://127.0.0.1/cgi-bin/hello.lua", {method:"POST", body: "hello"}).then(function(response) {
-//fetch("http://127.0.0.1/cgi-bin/hello.lua", {method:"POST"}).then(function(response) {
-  return response.text();
-}).then(function(data) {
-  console.log(data);
-}).catch(function(err) {
-  console.log('Fetch Error :-S', err);
-});
-
-
 document.body.appendChild(x)
