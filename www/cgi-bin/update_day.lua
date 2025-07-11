5a2fc41f4f11a466ccbb754f12a676c88784dc33
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-11 13:35:35 +0300

    update_day.lua: progress

--- www/cgi-bin/update_day.lua
+++ www/cgi-bin/update_day.lua
@@ -16,15 +16,25 @@ local main = function()
 	if common.validate_day(day) then
 		s = {}
 		table.insert(s, "BEGIN TRANSACTION")
-		-- TODO: DELETE rule_instance
-		-- TODO: DELETE day
+		table.insert(s, "DELETE FROM rule_instance WHERE day_id = '" .. day.id .. "'")
+		table.insert(s, "DELETE FROM day WHERE id = '" .. day.id .. "'")
 		local notes = "NULL"
 		if day.notes then
 			notes = "'" .. database:escape(day.notes) .. "'"
 		end
-		-- TODO: INSERT day
-		-- TODO: INSERT rule_instance, COMMIT
+		table.insert(s, "INSERT OR ROLLBACK INTO day ( id, notes) VALUES ( '" .. day.id .. "', " .. notes .. ')')
+		for _, rule_instance in ipairs(day.rule_instances) do
+			-- TODO: refactor into a function
+			local q = {}
+			table.insert(q, "INSERT OR ROLLBACK INTO rule_instance")
+			table.insert(q, "(rule_name, day_id, done, order_priority) VALUES (")
+			table.insert(q, "'" .. database:escape(rule_instance.rule_name) .. "',")
+			table.insert(q, string.format("'%s',%d,%d)", day.id, rule_instance.done, rule_instance.order_priority))
+			table.insert(s, table.concat(q, " "))
+		end
 		table.insert(s, "COMMIT")
+
+		-- TODO: execute
 
 		common.respond("true")
 	else
