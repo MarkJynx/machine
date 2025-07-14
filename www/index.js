ec1f28d9693c81b26bf85da8f64b8089d67bc95a
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-14 23:41:13 +0300

    reorder tasks

--- www/index.js
+++ www/index.js
@@ -8,35 +8,35 @@ fetch('cgi-bin/read_day.lua', {
 }).then(response => response.json())
   .then(response => generate_day(response))
 
-// TODO  3: HTML: in case of "null", button "Create Day"
-// TODO  4: JS:   empty "Create Day" handler
-// TODO  5: JS:   call create_day.lua
-// TODO  6: JS:   sort tasks by order_priority and print them out
-// TODO  7: HTML: table of unchecked checkbox tasks
-// TODO  8: HTML: add button "Save"
-// TODO  9: HTML: add button "Delete"
-// TODO 10: JS:   add empty "Delete" handler
-// TODO 11: JS:   call delete_day.lua
-// TODO 12: JS:   add empty "Save" handler
-// TODO 13: JS:   save handler: convert table to console.log
-// TODO 14: JS:   save handler: convert table to JSON
-// TODO 15: JS:   call update_day.lua
-// TODO 16: HTML: add navigation buttons ("Previous day", "Next day")
-// TODO 17: JS:   add empty navigation handlers
-// TODO 18: JS:   GET index.htm
-// TODO 19: JS:   index.js: read GET arguments
-// TODO 20: JS:   index.js: validate GET arguments
-// TODO 21: JS:   index.js: supersede current day in case of valid GET arguments
-// TODO 22: HTML: task_table_generation: add UP and DOWN buttons
-// TODO 23: JS:   task_table_generatino: add UP and DOWN button empty handlers
-// TODO 24: JS:   task_table_generation: add UP and DOWN button full handlers
-// TODO 25: HTML: task_table_generation: add DELETE button
-// TODO 26: JS:   task_table_generation: add empty DELETE handler
-// TODO 27: JS:   task_table_generation: add proper DELETE handler
-// TODO 28: HTML: add ADD_TASK button
-// TODO 29: HTML: add ADD_TASK dropdown
+// TODO  1: SQL:  fill up rule_instance table for day 2025-07-14 with tier 1 rules
+// TODO  2: JS:   index.js: read GET arguments
+// TODO  3: JS:   index.js: validate GET arguments
+// TODO  4: JS:   index.js: supersede current day in case of valid GET arguments
+// TODO  5: HTML: table of tasks
+// TODO  6: HTML: task_table_generation: add UP and DOWN buttons
+// TODO  7: HTML: task_table_generation: add DELETE button
+// TODO  9: HTML: add ADD_TASK button
+// TODO 10: HTML: add ADD_TASK dropdown
+// TODO 11: HTML: add button "Save"
+// TODO 12: HTML: add button "Delete"
+// TODO 13: HTML: add day.NOTES with payload already
+// TODO 14: HTML: add navigation buttons ("Previous day", "Next day")
+// TODO 15: HTML: in case of "null", button "Create Day"
+// TODO 16: JS:   task_table_generatino: add UP and DOWN button empty handlers
+// TODO 17: JS:   task_table_generation: add UP and DOWN button full handlers
+// TODO 18: JS:   task_table_generation: add empty DELETE handler
+// TODO 19: JS:   task_table_generation: add proper DELETE handler
+// TODO 20: JS:   add empty navigation handlers
+// TODO 21: JS:   GET index.htm
+// TODO 22: JS:   add empty "Delete" handler
+// TODO 23: JS:   call delete_day.lua
+// TODO 24: JS:   empty "Create Day" handler
+// TODO 25: JS:   call create_day.lua
+// TODO 26: JS:   add empty "Save" handler
+// TODO 27: JS:   save handler: convert table to console.log
+// TODO 28: JS:   save handler: convert table to JSON
+// TODO 29: JS:   call update_day.lua
 // TODO 30: JS:   add ADD_TASK button empty handler
 // TODO 31: JS:   add ADD_TASK button console handler
 // TODO 32: JS:   add ADD_TASK button proper handler (attach to end of the list)
-// TODO 33: HTML: add day.NOTES with payload already
-// TODO 34: JS:   save day.NOTES with payload
+// TODO 33: JS:   save day.NOTES with payload
