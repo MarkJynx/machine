02048c6bbe7d498c817a0bba51630efe8ad0236b
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-15 02:45:11 +0300

    HTML to add a task

--- www/head.js
+++ www/head.js
@@ -1,8 +1,64 @@
-function generate_day(day) {
+function generate_day(day, rules) {
 	document.body.replaceChildren()
 	if (day == null) {
 		document.body.textContent = "null"
 	} else {
-		document.body.textContent = "not null"
+		console.log(day)
+		let task_table = document.createElement("table")
+		for (i = 0; day.rule_instances != null && i < day.rule_instances.length; i++) {
+			let rule_name = day.rule_instances[i].rule_name
+			let done = day.rule_instances[i].done
+
+			let row = task_table.insertRow()
+			row.id = rule_name
+
+			let cell = row.insertCell()
+			let button = document.createElement("input")
+			button.type = "button"
+			button.value = "❌"
+			cell.appendChild(button)
+
+			cell = row.insertCell()
+			button = document.createElement("input")
+			button.type = "button"
+			button.value = "↑"
+			cell.appendChild(button)
+
+			cell = row.insertCell()
+			button = document.createElement("input")
+			button.type = "button"
+			button.value = "↓"
+			cell.appendChild(button)
+
+			cell = row.insertCell()
+			let checkbox = document.createElement("input")
+			checkbox.type = "checkbox"
+			checkbox.checked = Boolean(done)
+			cell.appendChild(checkbox)
+
+			cell = row.insertCell()
+			cell.innerText = rule_name
+			console.log(rule_name)
+		}
+
+		let row = task_table.insertRow()
+		let cell = row.insertCell()
+		cell = row.insertCell()
+		cell = row.insertCell()
+		cell = row.insertCell()
+		let button = document.createElement("input")
+		button.type = "button"
+		button.value = "+"
+		cell.appendChild(button)
+		cell = row.insertCell()
+		let selection = document.createElement("select")
+		for (let i = 0; rules != null && i < rules.length; i++) {
+			let option = document.createElement("option")
+			option.text = rules[i].name
+			selection.add(option)
+		}
+		cell.appendChild(selection)
+
+		document.body.appendChild(task_table)
 	}
 }
--- www/index.js
+++ www/index.js
@@ -9,39 +9,44 @@ const current_date = new Date()
 const current_date_string = current_date.toISOString().substring(0, 10)
 specified_date = specified_date ? specified_date : current_date_string;
 
-// TODO: enforce JSON schema
-fetch('cgi-bin/read_day.lua', {
-  method: 'POST',
-  headers: { 'Content-Type': 'text/plain' },
-  body: specified_date
-}).then(response => response.json())
-  .then(response => generate_day(response))
+async function main() {
+	// TODO: handle errors, validate with JSON schema
+	let day = await fetch('cgi-bin/read_day.lua', {
+	  method: 'POST',
+	  headers: { 'Content-Type': 'text/plain' },
+	  body: specified_date
+	})
+	let dayData = await day.json()
 
-// TODO  1: HTML: table of tasks
-// TODO  2: HTML: task_table_generation: add UP and DOWN buttons
-// TODO  3: HTML: task_table_generation: add DELETE button
-// TODO  4: HTML: add ADD_TASK button
-// TODO  5: HTML: add ADD_TASK dropdown
-// TODO  6: HTML: add button "Save"
-// TODO  7: HTML: add button "Delete"
-// TODO  8: HTML: add day.NOTES with payload already
-// TODO  9: HTML: add navigation buttons ("Previous day", "Next day")
-// TODO 10: HTML: in case of "null", button "Create Day"
-// TODO 11: JS:   task_table_generatino: add UP and DOWN button empty handlers
-// TODO 12: JS:   task_table_generation: add UP and DOWN button full handlers
-// TODO 13: JS:   task_table_generation: add empty DELETE handler
-// TODO 14: JS:   task_table_generation: add proper DELETE handler
-// TODO 15: JS:   add empty navigation handlers
-// TODO 16: JS:   GET index.htm
-// TODO 17: JS:   add empty "Delete" handler
-// TODO 18: JS:   call delete_day.lua
-// TODO 19: JS:   empty "Create Day" handler
-// TODO 20: JS:   call create_day.lua
-// TODO 21: JS:   add empty "Save" handler
-// TODO 22: JS:   save handler: convert table to console.log
-// TODO 23: JS:   save handler: convert table to JSON
-// TODO 24: JS:   call update_day.lua
-// TODO 25: JS:   add ADD_TASK button empty handler
-// TODO 26: JS:   add ADD_TASK button console handler
-// TODO 27: JS:   add ADD_TASK button proper handler (attach to end of the list)
-// TODO 28: JS:   save day.NOTES with payload
+	// TODO: handle errors, validate with JSON schema
+	let rules = await fetch('cgi-bin/read_rules.lua')
+	let rulesData = await rules.json()
+
+	generate_day(dayData, rulesData)
+}
+
+main()
+
+// TODO  1: HTML: add button "Save"
+// TODO  2: HTML: add button "Delete"
+// TODO  3: HTML: add day.NOTES with payload already
+// TODO  4: HTML: add navigation buttons ("Previous day", "Next day")
+// TODO  5: HTML: in case of "null", button "Create Day"
+// TODO  6: JS:   task_table_generatino: add UP and DOWN button empty handlers
+// TODO  7: JS:   task_table_generation: add UP and DOWN button full handlers
+// TODO  8: JS:   task_table_generation: add empty DELETE handler
+// TODO  9: JS:   task_table_generation: add proper DELETE handler
+// TODO 10: JS:   add empty navigation handlers
+// TODO 11: JS:   GET index.htm
+// TODO 12: JS:   add empty "Delete" handler
+// TODO 13: JS:   call delete_day.lua
+// TODO 14: JS:   empty "Create Day" handler
+// TODO 15: JS:   call create_day.lua
+// TODO 16: JS:   add empty "Save" handler
+// TODO 17: JS:   save handler: convert table to console.log
+// TODO 18: JS:   save handler: convert table to JSON
+// TODO 19: JS:   call update_day.lua
+// TODO 20: JS:   add ADD_TASK button empty handler
+// TODO 21: JS:   add ADD_TASK button console handler
+// TODO 22: JS:   add ADD_TASK button proper handler (attach to end of the list)
+// TODO 23: JS:   save day.NOTES with payload
