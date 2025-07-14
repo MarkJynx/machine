1345cd05b4714cc78615043e78873af8f319378e
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-14 21:20:24 +0300

    return to the project

--- www/index.js
+++ www/index.js
@@ -1,3 +1,42 @@
 let x = document.createElement("pre")
 x.textContent = "Hello World"
 document.body.appendChild(x)
+
+// TODO 1: determine what day is today
+// TODO 2: call read_day.lua
+// TODO 3: in case of "null", button "Create Day"
+// TODO 4: add empty (console.log) "Create Day" handler in head.js
+// TODO 5: call create_day.lua
+// TODO 6: sort tasks by order_priority and print them out
+// TODO 7: table of unchecked checkbox tasks
+// TODO 8: add button "Save"
+// TODO 9: add button "Delete"
+// TODO 10: add empty "Delete" handler
+// TODO 11: call delete_day.lua
+// TODO 12: add empty "Save" handler
+// TODO 13: save handler: convert table to console.log
+// TODO 14: save handler: convert table to JSON
+// TODO 15: call update_day.lua
+// TODO 16: add navigation buttons (previous day, next day)
+// TODO 17: add stub handlers (previous day, next day)
+// TODO 18: navigation: GET index.htm
+// TODO 19: index.js: read GET arguments
+// TODO 20: index.js: validate GET arguments
+// TODO 21: index.js: supersede current day in case of valid GET arguments
+// TODO 22: task_table_generation: add UP and DOWN buttons
+// TODO 23: task_table_generatino: add UP and DOWN button empty handlers
+// TODO 24: task_table_generation: add UP adn DOWN button full handlers
+// TODO 25: task_table_generation: add DELETE button
+// TODO 26: enfore REQUEST_METHOD env var
+// TODO 27: read_task.lua: stub
+// TODO 28: read_task.lua: enforce GET
+// TODO 29: read_task.lua: print out everything
+// TODO 30: read_task.lua: test and fix
+// TODO 32: call read_task.lua
+// TODO 31: add ADD_TASK button
+// TODO 33: add ADD_TASK dropdown
+// TODO 34: add ADD_TASK button empty handler
+// TODO 35: add ADD_TASK button console handler
+// TODO 36: add ADD_TASK button proper handler (attach to end of the list)
+// TODO 37: add day.NOTES with payload already
+// TODO 38: save day.NOTES with payload
