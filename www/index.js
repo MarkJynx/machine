023e480e3c7268aedd900a50e269305a2243cc08
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-14 21:31:52 +0300

    todo

--- www/index.js
+++ www/index.js
@@ -2,41 +2,43 @@ let x = document.createElement("pre")
 x.textContent = "Hello World"
 document.body.appendChild(x)
 
-// TODO 1: determine what day is today
-// TODO 2: call read_day.lua
-// TODO 3: in case of "null", button "Create Day"
-// TODO 4: add empty (console.log) "Create Day" handler in head.js
-// TODO 5: call create_day.lua
-// TODO 6: sort tasks by order_priority and print them out
-// TODO 7: table of unchecked checkbox tasks
-// TODO 8: add button "Save"
-// TODO 9: add button "Delete"
-// TODO 10: add empty "Delete" handler
-// TODO 11: call delete_day.lua
-// TODO 12: add empty "Save" handler
-// TODO 13: save handler: convert table to console.log
-// TODO 14: save handler: convert table to JSON
-// TODO 15: call update_day.lua
-// TODO 16: add navigation buttons (previous day, next day)
-// TODO 17: add stub handlers (previous day, next day)
-// TODO 18: navigation: GET index.htm
-// TODO 19: index.js: read GET arguments
-// TODO 20: index.js: validate GET arguments
-// TODO 21: index.js: supersede current day in case of valid GET arguments
-// TODO 22: task_table_generation: add UP and DOWN buttons
-// TODO 23: task_table_generatino: add UP and DOWN button empty handlers
-// TODO 24: task_table_generation: add UP adn DOWN button full handlers
-// TODO 25: task_table_generation: add DELETE button
-// TODO 26: enfore REQUEST_METHOD env var
-// TODO 27: read_task.lua: stub
-// TODO 28: read_task.lua: enforce GET
-// TODO 29: read_task.lua: print out everything
-// TODO 30: read_task.lua: test and fix
-// TODO 32: call read_task.lua
-// TODO 31: add ADD_TASK button
-// TODO 33: add ADD_TASK dropdown
-// TODO 34: add ADD_TASK button empty handler
-// TODO 35: add ADD_TASK button console handler
-// TODO 36: add ADD_TASK button proper handler (attach to end of the list)
-// TODO 37: add day.NOTES with payload already
-// TODO 38: save day.NOTES with payload
+// TODO  1: JS:   determine what day is today
+// TODO  2: JS:   call read_day.lua
+// TODO  3: HTML: in case of "null", button "Create Day"
+// TODO  4: JS:   empty "Create Day" handler
+// TODO  5: JS:   call create_day.lua
+// TODO  6: JS:   sort tasks by order_priority and print them out
+// TODO  7: HTML: table of unchecked checkbox tasks
+// TODO  8: HTML: add button "Save"
+// TODO  9: HTML: add button "Delete"
+// TODO 10: JS:   add empty "Delete" handler
+// TODO 11: JS:   call delete_day.lua
+// TODO 12: JS:   add empty "Save" handler
+// TODO 13: JS:   save handler: convert table to console.log
+// TODO 14: JS:   save handler: convert table to JSON
+// TODO 15: JS:   call update_day.lua
+// TODO 16: HTML: add navigation buttons ("Previous day", "Next day")
+// TODO 17: JS:   add empty navigation handlers
+// TODO 18: JS:   GET index.htm
+// TODO 19: JS:   index.js: read GET arguments
+// TODO 20: JS:   index.js: validate GET arguments
+// TODO 21: JS:   index.js: supersede current day in case of valid GET arguments
+// TODO 22: HTML: task_table_generation: add UP and DOWN buttons
+// TODO 23: JS:   task_table_generatino: add UP and DOWN button empty handlers
+// TODO 24: JS:   task_table_generation: add UP and DOWN button full handlers
+// TODO 25: HTML: task_table_generation: add DELETE button
+// TODO 26: JS:   task_table_generation: add empty DELETE handler
+// TODO 27: JS:   task_table_generation: add proper DELETE handler
+// TODO 28: LUA:  enfore REQUEST_METHOD env var
+// TODO 29: LUA:  read_task.lua: stub
+// TODO 30: LUA:  read_task.lua: enforce GET
+// TODO 31: LUA:  read_task.lua: print out everything
+// TODO 32: LUA:  read_task.lua: test and fix
+// TODO 33: LUA:  call read_task.lua
+// TODO 34: HTML: add ADD_TASK button
+// TODO 35: HTML: add ADD_TASK dropdown
+// TODO 36: JS:   add ADD_TASK button empty handler
+// TODO 37: JS:   add ADD_TASK button console handler
+// TODO 38: JS:   add ADD_TASK button proper handler (attach to end of the list)
+// TODO 39: HTML: add day.NOTES with payload already
+// TODO 40: JS:   save day.NOTES with payload
