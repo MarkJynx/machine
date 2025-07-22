e31e0c25ea662705142e814fa274231a0c565428
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-22 04:59:58 +0300

    read_matrix.lua: further progress

--- www/cgi-bin/read_matrix.lua
+++ www/cgi-bin/read_matrix.lua
@@ -22,6 +22,10 @@ local extract_extreme_day = function(database, order)
 	return nil
 end
 
+local extract_done = function(database)
+	-- TODO: make done[rule][date] lookup table and integrate it
+end
+
 local extract_days = function(database)
 	local lookup_table = {}
 	local days = common.collect_database(database, "SELECT * FROM day")
@@ -37,16 +41,46 @@ local add_days = function(date, days)
 	return string.format("%04d-%02d-%02d", t2.year, t2.month, t2.day)
 end
 
+local find_rule_schedule = function(date, schedules)
+	--1. 6. 2. 1. Check if applicable rule schedule exists. If not, fill with -1 and go to [2.6.2].
+	return nil
+end
+
+local rule_is_done = function(date, instances)
+	-- TODO: use done[rule][date] lookup table
+	for _, instance in ipairs(instances or {}) do
+		if instance.done and instance.day_id == date then
+			return true
+		end
+	end
+	return false
+end
+
+local rule_is_mandatory = function(date, schedule, instances)
+	-- TODO: use done[rule][date] lookup table (check for up to schedule.period iterations)
+	--1. 6. 2. 2. Check if mandatory (iterate over rule schedules & (last) instances).
+	return false
+end
+
 local process_day_rule = function(row, date, rule, days)
 	if not days[date] then
 		table.insert(row, -1)
 		return
 	end
 
-	--1. 6. 2. 1. Check if applicable rule schedule exists. If not, fill with -1 and go to [2.6.2].
-	--1. 6. 2. 2. Check if mandatory (iterate over rule schedules & (last) instances).
-	--1. 6. 2. 3. Check if done (iterate over rule instances).
-	--1. 6. 2. 4. Return anything from b00, b01, b10, b11
+	local schedule = find_rule_schedule(date, rule.rule_schedules)
+	if not schedule then
+		table.insert(row, -1)
+		return
+	end
+
+	local done = rule_is_done(date, rule.rule_instances)
+	local mandatory = rule_is_mandatory(date, schedule, rule.rule_instances)
+
+	local result = 0
+	result = done and result + 1 or result
+	result = mandatory and result + 2 or result
+	return result
 end
 
 local process_day = function(matrix, date, rules, days)
