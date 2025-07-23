2868ffc28594eeacd47c6682e1b33dd5c4a5788d
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-23 15:13:53 +0300

    read_matrix.lua: fix bugs

--- www/cgi-bin/read_matrix.lua
+++ www/cgi-bin/read_matrix.lua
@@ -12,9 +12,9 @@ end
 
 local extract_rule_done_lookup_table = function(rule)
 	local lookup_table = {}
-	for _, instance in ipairs(rule.rule_instances or {}) do
+	for _, instance in ipairs(rule.instances or {}) do
 		if instance.done then
-			lookup_table[rule.day_id] = true
+			lookup_table[instance.day_id] = true
 		end
 	end
 	return lookup_table
@@ -34,9 +34,9 @@ local extract_rule_schedule_lookup_table_schedule = function(lt, done_lt, schedu
 	end
 
 	local day_count = common.datediff(stop_date, start_date) + 1
-	local not_done_streak = 1
+	local not_done_streak = 999 -- TODO: use token value
 	for i = 1, day_count do
-		local current_date = add_days(start_date, i)
+		local current_date = add_days(start_date, i - 1)
 		local date_weekday = common.dateweekday(current_date)
 
 		if schedule.period <= not_done_streak and rule_schedule_weekdays[date_weekday] then
@@ -52,7 +52,7 @@ end
 
 local extract_rule_schedule_lookup_table = function(rule, first_day, last_day)
 	local lookup_table = {}
-	for _, schedule in ipairs(rule.rule_schedules or {}) do
+	for _, schedule in ipairs(rule.schedules or {}) do
 		extract_rule_schedule_lookup_table_schedule(lookup_table, rule.done_lt, schedule, first_day, last_day)
 	end
 	return lookup_table
@@ -61,8 +61,11 @@ end
 local extract_rules = function(database, first_day, last_day)
 	local rules = common.collect_database(database, "SELECT * FROM rule ORDER BY order_priority ASC")
 	for _, rule in ipairs(rules or {}) do
-		rule.schedules = common.collect_database(database, "SELECT * FROM rule_schedule ORDER BY JULIANDAY(start_date)")
-		rule.instances = common.collect_database(database, "SELECT * FROM rule_instance ORDER BY JULIANDAY(day_id)")
+		local selector = "WHERE rule_name = '" .. database:escape(rule.name) .. "'"
+		local q1 = "SELECT * FROM rule_schedule " .. selector .. " ORDER BY JULIANDAY(start_date) ASC"
+		local q2 = "SELECT * FROM rule_instance " .. selector .. " AND done = 1 ORDER BY JULIANDAY(day_id) ASC"
+		rule.schedules = common.collect_database(database, q1)
+		rule.instances = common.collect_database(database, q2)
 		rule.done_lt = extract_rule_done_lookup_table(rule)
 		rule.schedule_lt = extract_rule_schedule_lookup_table(rule, first_day, last_day)
 	end
@@ -93,15 +96,9 @@ local process_day_rule = function(row, date, rule, day_lt)
 		return
 	end
 
-	local schedule = rule.schedule_lt[date]
-	if not schedule then
-		table.insert(row, -1)
-		return
-	end
-
 	local done = rule.done_lt[date] and 1 or 0
 	local mandatory = rule.schedule_lt[date] and 2 or 0
-	return done + mandatory
+	table.insert(row, done + mandatory)
 end
 
 local process_day = function(matrix, date, rules, day_lt)
@@ -117,10 +114,10 @@ local main = function()
 	local database = common.open_database("cgi-bin/machine.db")
 	local first_day = extract_extreme_day(database, "ASC")
 	local last_day = extract_extreme_day(database, "DESC")
-	local rules = extract_rules(database)
-	local day_lt = extract_day_lookup_table(database)
 	local matrix = {}
 	if first_day and last_day then
+		local rules = extract_rules(database, first_day, last_day)
+		local day_lt = extract_day_lookup_table(database)
 		local day_count = common.datediff(last_day, first_day) + 1
 		for i = 1, day_count do
 			process_day(matrix, add_days(first_day, i - 1), rules, day_lt)
