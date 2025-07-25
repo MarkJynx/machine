59ff9c377a14cb33f4dc81ef74cb33950019e018
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-25 20:18:15 +0300

    start tracking again

--- init.sql
+++ init.sql
@@ -315,13 +315,13 @@ INSERT INTO rule (name, rule_category_name, rule_importance_label, description, 
 	21
 );
 
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Early rise",                "2025-07-14", NULL, 1, 127);
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Home cleaning",             "2025-07-14", NULL, 1, 127);
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Dental care (after sleep)", "2025-07-14", NULL, 1, 127);
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Shower",                    "2025-07-14", NULL, 2, 127);
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Hair care",                 "2025-07-14", NULL, 1, 127);
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Face shave",                "2025-07-14", NULL, 1, 127);
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Nail care",                 "2025-07-18", NULL, 7, 127);
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Workout: push",             "2025-07-18", NULL, 7, 127);
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Workout: pull",             "2025-07-19", NULL, 7, 127);
-INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Workout: legs",             "2025-07-20", NULL, 7, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Early rise",                "2025-07-25", NULL, 1, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Home cleaning",             "2025-07-25", NULL, 1, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Dental care (after sleep)", "2025-07-25", NULL, 1, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Shower",                    "2025-07-25", NULL, 2, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Hair care",                 "2025-07-25", NULL, 1, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Face shave",                "2025-07-25", NULL, 1, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Nail care",                 "2025-07-25", NULL, 7, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Workout: push",             "2025-07-25", NULL, 7, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Workout: pull",             "2025-07-26", NULL, 7, 127);
+INSERT INTO rule_schedule (rule_name, start_date, end_date, period, weekdays) VALUES ("Workout: legs",             "2025-07-27", NULL, 7, 127);
---
+++ www/cgi-bin/machine.sql
@@ -0,0 +1,8 @@
+INSERT INTO day (id) VALUES ("2025-07-25");
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ("Early rise",                 1, "2025-07-25", 0,  1);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ("Hair care",                  5, "2025-07-25", 1,  2);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ("Home cleaning",              2, "2025-07-25", 0,  3);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ("Shower",                     4, "2025-07-25", 0,  4);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ("Face shave",                 6, "2025-07-25", 0,  5);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ("Dental care (after sleep)",  3, "2025-07-25", 0,  6);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ("Workout: push",              8, "2025-07-25", 0,  7);
