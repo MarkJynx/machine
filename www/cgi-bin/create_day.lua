8f90f5d6fa5095c152bb7fb1de53fe91bd108fd0
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-27 10:46:14 +0300

    create_day.lua: refactor further

--- www/cgi-bin/create_day.lua
+++ www/cgi-bin/create_day.lua
@@ -7,7 +7,7 @@ local cjson = require("cjson.safe")
 
 local get_last_rule_instance = function(db, rule)
 	-- TODO: validate anything you get from database
-	local q = "SELECT * FROM rule_instance WHERE rule_name = '" .. rule.name .. "' AND done = 1 ORDER BY JULIANDAY(day_id) DESC LIMIT 1"
+	local q = "SELECT * FROM rule_instance WHERE rule_name = '" .. db:escape(rule.name) .. "' AND done = 1 ORDER BY JULIANDAY(day_id) DESC LIMIT 1"
 	return common.collect_single_record(db, q)
 end
 
@@ -22,11 +22,7 @@ local rule_applies = function(rule_schedule, last_rule_instance, date)
 		return false
 	end
 
-	if not last_rule_instance then
-		return true
-	end
-
-	if common.date_diff(date, last_rule_instance.day_id) < rule_schedule.period then
+	if last_rule_instance and common.date_diff(date, last_rule_instance.day_id) < rule_schedule.period then
 		return false
 	end
 
@@ -36,17 +32,18 @@ end
 local main = function()
 	local date = common.http_enforce_date_payload()
 	local db = common.open_database("cgi-bin/machine.db")
-	local response = "null"
-	if date and not common.day_exists(db, date) then
-		local rules = common.collect_database(db, "SELECT * FROM rule ORDER BY order_priority ASC") or {}
-		rules = totable(filter(function(r) return rule_applies(common.get_rule_schedule(db, r.name, date), get_last_rule_instance(db, r), date) end, rules))
-
-		local day = { id = date, rule_instances = {} }
-		each(function(r) table.insert(day.rule_instances, { rule_name = r.name, done = 0 }) end, rules)
-		response = cjson.encode(day) or "null"
+	if common.day_exists(db, date) then
+		db:close()
+		common.http_response(nil)
 	end
-	common.http_respond(response)
+
+	local rules = common.collect_database(db, "SELECT * FROM rule ORDER BY order_priority ASC") or {}
+	rules = totable(filter(function(r) return rule_applies(common.get_rule_schedule(db, r.name, date), get_last_rule_instance(db, r), date) end, rules))
 	db:close()
+
+	local day = { id = date, rule_instances = {} }
+	each(function(r) table.insert(day.rule_instances, { rule_name = r.name, done = 0 }) end, rules)
+	common.http_respond(cjson.encode(day))
 end
 
 main()
