a5ab5d5a01cbde20b51143d6b44495ce8c9de4fd
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-27 06:46:38 +0300

    create_day.lua: integrate fun

--- www/cgi-bin/create_day.lua
+++ www/cgi-bin/create_day.lua
@@ -1,12 +1,9 @@
 #!/usr/bin/env lua
 
+require("fun")()
 local common = require("cgi-bin.common")
 local cjson = require("cjson.safe")
 
-
-local get_rules = function(db)
-	return common.collect_database(db, "SELECT * FROM rule ORDER BY order_priority ASC")
-end
 
 local get_last_rule_instance = function(db, rule)
 	-- TODO: validate anything you get from database
@@ -38,23 +35,19 @@ end
 
 local main = function()
 	common.enforce_http_method("POST")
-	local payload = common.extract_valid_date_payload(common.extract_content_length())
-	local database = common.open_database("cgi-bin/machine.db")
+	local date = common.extract_valid_date_payload(common.extract_content_length())
+	local db = common.open_database("cgi-bin/machine.db")
 	local response = "null"
-	if payload and not common.day_exists(database, payload) then
-		local day = { id = payload, rule_instances = {} }
-		local rules = get_rules(database) or {}
-		for _, rule in ipairs(rules) do
-			local rule_schedule = common.get_rule_schedule(database, rule.name, payload)
-			local last_rule_instance = get_last_rule_instance(database, rule)
-			if rule_applies(rule_schedule, last_rule_instance, payload) then
-				table.insert(day.rule_instances, { rule_name = rule.name, done = 0 })
-			end
-		end
+	if date and not common.day_exists(db, date) then
+		local rules = common.collect_database(db, "SELECT * FROM rule ORDER BY order_priority ASC") or {}
+		rules = totable(filter(function(r) return rule_applies(common.get_rule_schedule(db, r.name, date), get_last_rule_instance(db, r), date) end, rules))
+
+		local day = { id = date, rule_instances = {} }
+	    each(function(r) table.insert(day.rule_instances, { rule_name = r.name, done = 0 }) end, rules)
 		response = cjson.encode(day) or "null"
 	end
 	common.respond(response)
-	database:close()
+	db:close()
 end
 
 main()
