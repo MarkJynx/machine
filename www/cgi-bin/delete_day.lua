d061ebb87bc3a95bbeab439c98e425b72838f22b
Mark Jynx <markjynx@users.noreply.github.com>
2025-07-28 10:55:30 +0300

    delete_day.lua: absolutely minimal remains

--- www/cgi-bin/delete_day.lua
+++ www/cgi-bin/delete_day.lua
@@ -5,10 +5,7 @@ local common = require("cgi-bin.common")
 
 local main = function()
 	local date = common.http_enforce_date_payload()
-	local response = "false"
-	if common.db_delete_day(date) then
-		response = "true"
-	end
+	local response = common.db_delete_day(date) and "true" or "false"
 	common.http_respond(response)
 	common.database_to_sql(database, "cgi-bin/machine.sql")
 end
