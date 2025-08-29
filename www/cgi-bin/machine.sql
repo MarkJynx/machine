d567db134ac7b36689c256920984bf6f92c1f006
Mark Jynx <markjynx@users.noreply.github.com>
2025-08-29 12:10:53 +0300

    begin tracking today

---
+++ www/cgi-bin/machine.sql
@@ -0,0 +1,7 @@
+INSERT INTO day (id) VALUES ('2025-08-29');
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Early rise',                 1, '2025-08-29', 1,  1);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Home cleaning',              2, '2025-08-29', 0,  2);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Shower',                     4, '2025-08-29', 0,  3);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Hair care',                  5, '2025-08-29', 0,  4);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Face shave',                 6, '2025-08-29', 0,  5);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Dental care (after sleep)',  3, '2025-08-29', 0,  6);
