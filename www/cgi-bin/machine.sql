d6a29c826173bb5fd2c9a91f9c5d654d289c332f
Mark Jynx <markjynx@users.noreply.github.com>
2025-10-02 09:39:47 +0300

    2025-10-02

--- www/cgi-bin/machine.sql
+++ www/cgi-bin/machine.sql
@@ -183,3 +183,12 @@ INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_prio
 INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: pull (B)',            9, '2025-10-01', 0,  8);
 INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (A)',            8, '2025-10-01', 1,  9);
 INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (B)',           11, '2025-10-01', 0, 10);
+INSERT INTO day (id) VALUES ('2025-10-02');
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Early rise',                   1, '2025-10-02', 1,  1);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Home cleaning',                2, '2025-10-02', 1,  2);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Hair care',                    5, '2025-10-02', 1,  3);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Face shave',                   6, '2025-10-02', 1,  4);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Dental care (after sleep)',    3, '2025-10-02', 1,  5);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: push (B)',           10, '2025-10-02', 1,  6);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: pull (B)',            9, '2025-10-02', 0,  7);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (B)',           11, '2025-10-02', 0,  8);
