7fc17987060f99af1271aa62c26b78dbc85ee8f1
Mark Jynx <markjynx@users.noreply.github.com>
2026-01-22 17:49:46 +0200

    new day

--- www/cgi-bin/machine.sql
+++ www/cgi-bin/machine.sql
@@ -824,3 +824,14 @@ INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_prio
 INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: pull (B)',            9, '2026-01-21', 0,  7);
 INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (A)',            8, '2026-01-21', 0,  8);
 INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (B)',           11, '2026-01-21', 0,  9);
+INSERT INTO day (id) VALUES ('2026-01-22');
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Early rise',                   1, '2026-01-22', 1,  1);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Home cleaning',                2, '2026-01-22', 1,  2);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Shower',                       4, '2026-01-22', 1,  3);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Hair care',                    5, '2026-01-22', 1,  4);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Face shave',                   6, '2026-01-22', 1,  5);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Dental care (after sleep)',    3, '2026-01-22', 1,  6);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: push (B)',           10, '2026-01-22', 1,  7);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: pull (B)',            9, '2026-01-22', 1,  8);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (A)',            8, '2026-01-22', 0,  9);
+INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (B)',           11, '2026-01-22', 0, 10);
