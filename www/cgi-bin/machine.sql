cb3df57e40f70f59392ed94d2bc7c134a52ea1ed
Mark Jynx <markjynx@users.noreply.github.com>
2026-03-03 09:13:15 +0200

    remove yesterday, as yesterday was a vacation day

--- www/cgi-bin/machine.sql
+++ www/cgi-bin/machine.sql
@@ -221,16 +221,3 @@ INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_prio
 INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: push (B)',           11, '2026-02-27', 0,  7);
 INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: pull (B)',           12, '2026-02-27', 0,  8);
 INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (B)',           13, '2026-02-27', 0,  9);
-INSERT INTO day (id) VALUES ('2026-03-02');
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Early rise',                   1, '2026-03-02', 1,  1);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Nail care',                    7, '2026-03-02', 1,  2);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Home cleaning',                2, '2026-03-02', 1,  3);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Shower',                       4, '2026-03-02', 1,  4);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Hair care',                    5, '2026-03-02', 1,  5);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Face shave',                   6, '2026-03-02', 1,  6);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Dental care (after sleep)',    3, '2026-03-02', 1,  7);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: push (B)',           11, '2026-03-02', 0,  8);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: pull (A)',            9, '2026-03-02', 0,  9);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: pull (B)',           12, '2026-03-02', 0, 10);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (A)',           10, '2026-03-02', 0, 11);
-INSERT INTO rule_instance (rule_name, rule_schedule_id, day_id, done, order_priority) VALUES ('Workout: legs (B)',           13, '2026-03-02', 0, 12);
