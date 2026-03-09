08b9a023c6cea71d437c3ca9a39054e638d03234
Mark Jynx <markjynx@users.noreply.github.com>
2026-03-09 11:35:09 +0200

    create_day.lua: refactor

--- www/cgi-bin/create_day.lua
+++ www/cgi-bin/create_day.lua
@@ -20,13 +20,9 @@ end
 local main = function()
 	local date = common.http_enforce_date_payload()
 	local shallow = common.db_read_shallow(date)
-	if shallow.day then
-		common.http_response(nil)
-	end
-
+	common.http_panic(shallow.day ~= nil)
 	local rules = totable(filter(function(r) return rule_applies(r, date) end, shallow.rules))
 	local day = { id = date, rule_instances = reduce(function(a, r) table.insert(a, {rule_name = r.name, done = 0}) return a end, {}, rules) }
-
 	common.http_respond(cjson.encode(day) or "null")
 end
 
