b3870c704d9c0813c4a8b2d502b6f7c13b288a85
Mark Jynx <markjynx@users.noreply.github.com>
2026-03-12 12:16:27 +0200

    add a note

--- init.sql
+++ init.sql
@@ -30,22 +30,10 @@ INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Home cleaning
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Shower", 4, 4, 127);
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Hair care", 5, 2, 127);
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Face shave", 6, 2, 127);
--- INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Face care", 7);
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Dental care (after sleep)", 8, 1, 127);
--- INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Fresh clothes", 9);
--- INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Productivity", 10);
--- INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Car care", 11);
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Workout: push (A)", 12, 7, 127);
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Workout: push (B)", 13, 7, 127);
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Workout: pull (A)", 14, 7, 127);
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Workout: pull (B)", 15, 7, 127);
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Workout: legs (A)", 16, 7, 127);
 INSERT INTO rule (name, order_priority, period, weekdays) VALUES ("Workout: legs (B)", 17, 7, 127);
--- INSERT INTO rule (name, priority, period, weekdays) VALUES ("Workout: core", 18);
--- INSERT INTO rule (name, priority, period, weekdays) VALUES ("Workout: cardio", 19);
--- INSERT INTO rule (name, priority, period, weekdays) VALUES ("Cooking", 20);
--- INSERT INTO rule (name, priority, period, weekdays) VALUES ("Laundry", 21);
--- INSERT INTO rule (name, priority, period, weekdays) VALUES ("Body care", 22);
--- INSERT INTO rule (name, priority, period, weekdays) VALUES ("Dental care (before sleep)", 23);
--- INSERT INTO rule (name, priority, period, weekdays) VALUES ("Homemade food", 24);
--- INSERT INTO rule (name, priority, period, weekdays) VALUES ("Diet", 25); -- TODO: probably should be merged with homemade food
